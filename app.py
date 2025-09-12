acbb17e5d88a775bfecfe44cede27595d1921898
zinebnadak <zinebnadak04@outlook.com>
2025-09-12 20:32:11 +0300

    10.9 (chroma db, fastapi and uvicorn added) new! updated render https line

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ question = st.text_input("Ask a question:")
 
 if st.button("Send") and question:
     with st.spinner("Getting answer..."):
-        response = requests.post("https://nadaks-ai-chatbot.onrender.com/ask", json={"question": question})
+        response = requests.post("https://nadak-s-ai-chatbot.onrender.com/ask", json={"question": question})
         if response.status_code == 200:
             answer = response.json().get("answer", "No answer found.")
             st.markdown(f"**Bot:** {answer}")
